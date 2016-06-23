1ab6a2d608c04abd444b09a445e819ed86c43f69
etworker <etworker@outlook.com>
2016-06-23 22:51:54 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,208 @@
-# LocalizationHelper
-读取已有的Android本地化资源文件strings.xml，应用到仅有中文的资源文件上，生成对应的英文资源文件
+# 本地化助手
+
+## 介绍
+
+假设曾经开发过A项目，也做过本地化，有这样2个本地化文件：
+
+`strings-zh.xml`
+```
+...
+<string name="screen_help">帮助</string>
+...
+```
+
+`strings-en.xml`
+```
+...
+<string name="screen_help">Help</string>
+...
+```
+
+现在开发B项目，希望能够自动复用A项目本地化的资源。
+如B项目有个本地化文件：`strings.xml`
+```
+...
+<string name="screen_help">帮助</string>
+...
+```
+
+应用自动本地化后，可以生成新的本地化文件，包含匹配过的结果，如下
+
+```
+...
+<string name="screen_help">Help</string>
+...
+```
+
+## 使用
+
+在Test目录下，有2个本地化文件，代表A项目已有的本地化文件，分别是：
+
+`strings-zh.xml`
+
+```
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+    <string name="device_list">设备列表</string>
+    <string name="bluetooth">蓝牙</string>
+    <string name="password_modify">修改密码</string>
+    <string name="screen_camera">照相机</string>
+    <string name="screen_camera_settings">相机设置</string>
+    <string name="screen_clock">闹钟</string>
+    <string name="screen_gallery">图库</string>
+    <string name="screen_contacts">联系人</string>
+    <string name="screen_contacts_exist">联系人已存在</string>
+    <string name="screen_app_detial">应用详情</string>
+    <string name="screen_app_permission">权限信息</string>
+    <string name="device_info">设备信息</string>
+    <string name="screen_calendar">日历</string>
+    <string name="screen_app_world">应用世界</string>
+    <string name="screen_help">帮助</string>
+    <string name="personal_info">个人信息</string>
+    <string name="personal_info_modify">修改个人信息</string>
+    <string name="personal_info_modify_failed">修改失败</string>
+    <string name="creator_qrcode">二维码</string>
+</resources>
+```
+
+和`strings-en.xml`
+
+```
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+    <string name="device_list">Device List</string>
+    <string name="bluetooth">Bluetooth</string>
+    <string name="password_modify">Modify Password</string>
+    <string name="screen_camera">Camera</string>
+    <string name="screen_camera_settings">Camera Setting</string>
+    <string name="screen_clock">Alarm Clock</string>
+    <string name="screen_gallery">Gallery</string>
+    <string name="screen_contacts">Contacts</string> 
+    <string name="screen_contacts_exist">Contact Exist</string>
+    <string name="screen_app_detial">App Detail</string>
+    <string name="screen_app_permission">App Permission</string>
+    <string name="device_info">Device Info</string>
+    <string name="screen_calendar">Calendar</string>
+    <string name="screen_app_world">App World</string>
+    <string name="screen_help">Help</string>
+    <string name="personal_info">Personal Info</string>
+    <string name="personal_info_modify">Modify Personal Info</string>
+     <string name="personal_info_modify_failed">Modify failed</string>
+    <string name="creator_qrcode">QR Code</string>
+</resources>
+```
+
+现在要处理的文件是`strings.xml`，内容如下：
+
+```
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+    <string name="device_list">设备列表</string>
+    <string name="bluetooth">蓝牙</string>
+    <string name="device_list">设备列表</string>
+    <string name="bluetooth">蓝牙</string>
+    <string name="password_modify">修改密码</string>
+    <string name="screen_clock">闹钟</string>
+    <string name="screen_gallery">图库</string>
+    <string name="screen_contacts">联系人</string>
+    <string name="screen_app_detial">应用详情</string>
+    <string name="personal_info">个人信息</string>
+    <string name="personal_info_modify">修改个人信息</string>
+    <string name="creator_qrcode">二维码</string>
+    
+    <!-- Server Error Text -->
+    <string name="server_connect_failed">无法连接到网络，请检查网络配置</string>
+    <string name="server_login_error">登录信息输入错误</string>
+    <string name="server_error_exist_contact">联系人已存在</string>
+    <string name="modify_failed">修改失败</string>
+</resources>
+```
+
+命令行下运行如下命令：
+
+```
+D:\Python\LocalizationHelper\Test> python ..\res_manager.py --dir . --csv map.csv --apply strings.xml
+```
+
+它的含义是读取当前目录下全部xml文件，保存其匹配关系到`map.csv`文件，并对`strings.xml`应用。
+最终结果是生成TAB作为分隔符的`map.csv`文件和本地化后的`strings.xml.new`文件。
+
+使用Excel打开`map.csv`文件的内容如下：
+
+![](map.png)
+
+本地化后的`strings.xml.new`文件内容如下：
+
+```
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+    <string name="device_list">Device List</string>
+    <string name="bluetooth">Bluetooth</string>
+    <string name="device_list">Device List</string>
+    <string name="bluetooth">Bluetooth</string>
+    <string name="password_modify">Modify Password</string>
+    <string name="screen_clock">Alarm Clock</string>
+    <string name="screen_gallery">Gallery</string>
+    <string name="screen_contacts">Contacts</string>
+    <string name="screen_app_detial">App Detail</string>
+    <string name="personal_info">Personal Info</string>
+    <string name="personal_info_modify">Modify Personal Info</string>
+    <string name="creator_qrcode">QR Code</string>
+    
+    <!-- Server Error Text -->
+    <string name="server_connect_failed">无法连接到网络，请检查网络配置</string>
+    <string name="server_login_error">登录信息输入错误</string>
+    <string name="server_error_exist_contact">Contact Exist</string>
+    <string name="modify_failed">Modify failed</string>
+</resources>
+```
+
+可见能处理的都处理了，但是剩余两条因为项目A中没有对应信息，所以残留。
+
+如果希望这两条记录也能自动处理，需要在命令行中加入`--supplement`参数。
+这个参数的目的是将`server_login_error`转为`server login error`这样的内容。
+
+命令行下运行如下命令：
+
+```
+D:\Python\LocalizationHelper\Test> python ..\res_manager.py --dir . --csv map.csv --apply strings.xml --supplement
+```
+
+这次生成的`map.csv`文件的内容如下，红色框中就是自动生成的：
+
+![](map2.png)
+
+本地化后的`strings.xml.new`文件内容如下：
+
+```
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+    <string name="device_list">Device List</string>
+    <string name="bluetooth">Bluetooth</string>
+    <string name="device_list">Device List</string>
+    <string name="bluetooth">Bluetooth</string>
+    <string name="password_modify">Modify Password</string>
+    <string name="screen_clock">Alarm Clock</string>
+    <string name="screen_gallery">Gallery</string>
+    <string name="screen_contacts">Contacts</string>
+    <string name="screen_app_detial">App Detail</string>
+    <string name="personal_info">Personal Info</string>
+    <string name="personal_info_modify">Modify Personal Info</string>
+    <string name="creator_qrcode">QR Code</string>
+    
+    <!-- Server Error Text -->
+    <string name="server_connect_failed">server connect failed</string>
+    <string name="server_login_error">server login error</string>
+    <string name="server_error_exist_contact">Contact Exist</string>
+    <string name="modify_failed">Modify failed</string>
+</resources>
+```
+
+如果资源ID命名规范，那么使用`--supplement`参数倒是方便，否则还是手动修改吧。
+
+使用中生成的csv文件也可以作为前期资源导入，只需要`--load map.csv`参数。
---
+++ res_manager.py
@@ -0,0 +1,250 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+import getopt
+
+
+# detect if chinese in word
+def contains_zh(word):
+    return re.compile(u'[\u4e00-\u9fa5]+').search(word)
+
+
+# resword is basic unit, save english word and chinese word
+class resword_info:
+    def __init__(self):
+        self.res_id = ''
+        self.words = []
+
+
+    def add_word(self, word):
+        if word in self.words:
+            return
+
+        # append chinese at tail, and insert english at head
+        if contains_zh(word):
+            if len(self.words) == 0:
+                self.words.append('')
+            self.words.append(word)
+        else:
+            if len(self.words) == 0:
+                self.words.append(word)
+            else:
+                if self.words[0] == '':
+                    self.words[0] = word
+                else:
+                    self.words.insert(0, word)
+
+    def to_string(self):
+        str = self.res_id
+        return u'%s\t%s' % (self.res_id, '\t'.join(self.words))
+
+
+# res manager
+class res_manager:
+    def __init__(self):
+        self.reswords = {}
+        self.reverse_map = {}  # for map chinese to english
+
+
+    def add_resword(self, res_id, word):
+        if self.reswords.has_key(res_id):
+            self.reswords[res_id].add_word(word)
+        else:
+            ri = resword_info()
+            ri.res_id = res_id
+            ri.add_word(word)
+            self.reswords[res_id] = ri
+
+    def read_line(self, line):
+        parts = line.split('\t')
+        if len(parts) > 2:
+            res_id = parts[0]
+            for word in parts[1:]:
+                self.add_resword(res_id, word)
+
+    def is_xml_file(self, xml_filename):
+        return xml_filename.lower().endswith('.xml')
+
+    def read_xml(self, xml_filename):
+        if not self.is_xml_file(xml_filename):
+            # print u'%s is not xml file' % (xml_filename)
+            return False
+
+        print 'read_xml ' + xml_filename
+        fi = open(xml_filename, "r")
+        for line in fi.readlines():
+            line = line.decode('utf-8').strip()
+            # <string name="msg_wifipwd_error">Wrong password</string>
+            p = re.compile(u'<string name="([\w\-_]+)">(.*?)</string>')
+            m = p.search(line)
+            if m:
+                self.add_resword(m.group(1), m.group(2))
+
+        fi.close()
+        return True
+
+    # read all xml files in dir
+    def read_dir(self, dir):
+        files = os.listdir(dir)
+        for file in files:
+            self.read_xml(dir + '\\' + file)
+
+    # export reswords to csv file
+    def write_csv(self, csv_filename):
+        fo = open(csv_filename, "w")
+
+        for (res_id, resword) in self.reswords.items():
+            fo.write(''.encode('utf-8-sig'))
+            line = resword.to_string().encode('utf-8')
+            fo.write(line)
+            fo.write('\n')
+
+        fo.close()
+        return True
+
+    # import exist csv file to enhance resword
+    def read_csv(self, csv_filename):
+        fi = open(csv_filename, "r")
+        try:
+            for line in fi.readlines():
+                line = line.decode('utf-8').strip()
+                self.read_line(line)
+
+            return True
+        finally:
+            fi.close()
+
+        return False
+
+    # for those which has no exist localization word, simply replace "_" with " " as supplement
+    def supplement(self):
+        for (res_id, ri) in self.reswords.items():
+            if (len(ri.words) > 1) and (ri.words[0] == ''):
+                # e.g. take_a_picture -> take a picture
+                ri.words[0] = res_id.replace('_', ' ')
+
+    # map from chinese to english
+    def get_reverse_map(self):
+        for (res_id, ri) in self.reswords.items():
+            if (len(ri.words) > 1) and (ri.words[0] != '') and (ri.words[1] != ''):
+                if contains_zh(ri.words[1]):
+                    self.reverse_map[ri.words[1]] = ri.words[0]
+
+    # for those which has no exist localization word, use translated chinese to match english word
+    def reverse_supplement(self):
+        self.get_reverse_map()
+
+        for (res_id, ri) in self.reswords.items():
+            if (len(ri.words) > 1) and (ri.words[0] == '') and (ri.words[1] != ''):
+                if self.reverse_map.has_key(ri.words[1]):
+                    ri.words[0] = self.reverse_map[ri.words[1]]
+                    self.reswords[res_id] = ri
+
+    # apply localization to this xml file and write to another file with ".new" appended
+    def apply_to_xml(self, xml_filename):
+        print 'apply_to_xml ' + xml_filename
+
+        if not os.path.exists(xml_filename):
+            print u'%s is not exist' % (xml_filename)
+            return False
+
+        fi = open(xml_filename, "r")
+        fo = open(xml_filename + '.new', "w")
+        fo.write(''.encode('utf-8-sig'))
+
+        for line in fi.readlines():
+            newline = line.decode('utf-8')
+            # <string name="msg_wifipwd_error">Wrong password</string>
+            p = re.compile(u'<string name="([\w\-_]+)">(.*?)</string>')
+            m = p.search(newline)
+            if m:
+                res_id = m.group(1)
+                word = m.group(2)
+                if contains_zh(word):
+                    if self.reswords.has_key(res_id):
+                        newline = newline.replace(word, self.reswords[res_id].words[0])
+                    else:
+                        if self.reverse_map.has_key(word):
+                            newline = newline.replace(word, self.reverse_map[word])
+
+            fo.write(newline.encode('utf-8'))
+
+        fi.close()
+        fo.close()
+        return True
+
+
+def print_usage():
+    this_name = os.path.split(sys.argv[0])[1]
+    print "Usage example:"
+    print "python %s -l d:\\a.csv -d d:\\test -x d:\\a.xml -c d:\\b.csv -a d:\\d.xml -s" % (this_name)
+    print "or"
+    print "python %s --load d:\\a.csv --dir d:\\test --xml d:\\a.xml --csv d:\\b.csv --apply d:\\d.xml --supplement" % (
+        this_name)
+
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hsd:x:c:l:a:",
+                                   ["help", "supplement", "dir=", "xml=", "csv=", "load=", "apply="])
+    except getopt.GetoptError, err:
+        print str(err)
+        print_usage()
+        sys.exit(2)
+
+    dirs = []
+    xmls = []
+    csv = ''
+    load_csvs = []
+    apply_xmls = []
+    supplement = False
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print_usage()
+            sys.exit()
+        elif o in ("-d", "--dir"):
+            dirs.append(a)
+        elif o in ("-x", "--xml"):
+            xmls.append(a)
+        elif o in ("-c", "--csv"):
+            csv = a
+        elif o in ("-l", "--load"):
+            load_csvs.append(a)
+        elif o in ("-a", "--apply"):
+            apply_xmls.append(a)
+        elif o in ("-s", "--supplement"):
+            supplement = True
+        else:
+            assert False, "unhandled option"
+            sys.exit(2)
+
+    rm = res_manager()
+
+    if len(load_csvs) > 0:
+        for c in load_csvs:
+            rm.read_csv(c)
+
+    for d in dirs:
+        rm.read_dir(d)
+
+    for x in xmls:
+        rm.read_xml(x)
+
+    rm.reverse_supplement()
+
+    if supplement:
+        rm.supplement()
+
+    if csv != '':
+        rm.write_csv(csv)
+
+    if len(apply_xmls) > 0:
+        for a in apply_xmls:
+            rm.apply_to_xml(a)
+
+
+if __name__ == "__main__":
+    main()
